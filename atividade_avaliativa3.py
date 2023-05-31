983b29860c5209d7163e45d336f1691e0dbe62b4
eduardodrago <128647036+eduardodrago@users.noreply.github.com>
2023-05-31 18:41:10 -0300

    Add files via upload

---
+++ atividade_avaliativa3.py
@@ -0,0 +1,4 @@
+#Objetivo: Criar um programa que organize uma lista de nomes em ordem alfabetica
+#Bonus: Organizar por ordem alfabetica da ultima letra do nome
+lista=["Alexandre","Alice","André","Arthur","Arthur","Artur","Augusto","Bernardo","Bernardo","Bruno","Davi","Diego","Eduardo","Fabrício","Felipe","Fernando","Francisco","Francisco","Gabriel","Gabriel","Giovanna","Giovanni","Guilherme","Guilherme","Hector","Henrique","Inácio","João","João","Joaquim","Júlia","Lauren","Leonardo","Leonardo","Lucas","Lucas","Marina","Matheus","Matheus","Paula","Pedro","Pedro","Pedro","Pedro","Rafael","Regis","Sofia","Stella","Thiago","Valentina","Vicente"]
+print(sorted(lista))
